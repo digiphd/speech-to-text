0ba5a52d03cf5c90ca19b5255779fe3fa2d28127
Roger <roger@rscreativetech.com>
2022-12-21 11:47:40 +1000

    Updated Readme and streamlined main.py

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 
 # WHAT IS THIS APP?
+See running app right here:
 
+This is for anyone wanting to transcribe audio from an .mp3 or .mp4 file no longer than 10mins. 
+
+It is completely free to use thanks to openAPI Whisper model. 
+
+Note: Larger files do take a while to process. So I recommend keeping the file as small as possible, ideally less than 10MB.
 
 
 
 # WHAT I LEARNED
+- Streamlit
+- OpenAI Whisper
 
-# HOW TO RUN
+# INSTALLATION LOCALLY (MacOS)
+
+Step 1: 
+`pip install streamlit`
+Step 2:
+`pip install git+https://github.com/openai/whisper.git`
+Step 3: 
+`streamlit run main.py`
 
 
-# TROUBLE SHOOTING
+## TROUBLE SHOOTING
 If you get this error on MacOS:
 `ERROR: Could not build wheels for backports.zoneinfo which use PEP 517 and cannot be installed directly`
 
@@ -18,13 +33,5 @@ You must follow these two steps:
 1. `export C_INCLUDE_PATH=/Library/Developer/CommandLineTools/Library/Frameworks/Python3.framework/Versions/3.8/Headers`
 2. `pip install backports.zoneinfo`
 
-
-### TODO: Day 1
-- [ ] Setup UI Layout
-- [ ] Setup GitHub Repositiory
-- [ ] Get speech to text
-
-### TODO: Day 2
-- [ ] Work on Sentiment Analysis
 
 
--- main.py
+++ main.py
@@ -1,28 +1,57 @@
 import streamlit as st
+import os
+from tempfile import NamedTemporaryFile
+from bin.audio2text import Speech2Text
 
-st.set_page_config(
-    page_title="Instructions",
-    page_icon="👋",
-)
+st.set_page_config(page_title="Transcribe", page_icon="📈")
+st.markdown("#🔊 Transcribe an Audio File")
+st.markdown("""
+            - Upload a wav, mp3 or mp4 file 
+            - Press Transcribe 
+            - Review and edit
+            - Download txt file
+            Recommended File Length: less than 10minutes.
+            """)
 
-st.write('# Welcome to Prod Tools!')
 
-st.sidebar.success("Select a tool above.")
+c1, c2, c3 = st.columns([1, 4, 1])
 
-st.markdown(
-    """
-    Prod Tools is an open-source collection of commonly used tools in my day built specifically for
-    digital marketers, VA's and/or developers who just want to go to one place.
+with c2:
+    with st.form(key="my_form"):
+        f = st.file_uploader("Choose a file")
+        st.info("""Upload a .wav file above.""")
+        submit_button = st.form_submit_button(label="Transcribe")
 
-    **👈 Select a tool from the dropdown on the left**!
+if f is not None:
 
-    ### Tools include
+    path_in = f.name
+    # Get file size from buffer
+    # Source: https://stackoverflow.com/a/19079887
+    old_file_position = f.tell()
+    f.seek(0, os.SEEK_END)
+    getsize = f.tell()  # os.path.getsize(path_in)
+    f.seek(old_file_position, os.SEEK_SET)
+    getsize = round((getsize / 1000000), 1)
 
-    - Transcribe - Convert audio to text
-    - Social Sentiment - Enter a hashtag or topic, see what the general public sentiment is
-    - Inspirational Quotes - Get inspired
-    - Compress images - batch compress images
-    - Pomodoro Timer - Get productive
-    - Image Background Remove
-"""
-)
+    with NamedTemporaryFile(dir='.', suffix='.csv') as file:
+        file.write(f.getbuffer())
+        path = file.name
+        Whisper = Speech2Text()
+        text = Whisper.transcribe_audio(path)
+
+    if text:
+        st.audio(f)
+        input = st.text_area("Edit Your Transcription", value=text)
+        # st.form_submit_button(label="Download")
+        st.warning(
+         "🚨 Before pressing Download, make sure you save by pressing CMD+Enter.")
+        st.download_button("Download",
+                            input,
+                           file_name=None,
+                            mime=None,
+                            key=None,
+                            help=None,
+                            on_click=None,
+                            args=None,
+                            kwargs=None,
+                        )
